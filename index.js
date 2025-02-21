fd0bdaa3604c2ad5b69aefc1a56d8769dd644441
toxickritik <nikitarogozin414@gmail.com>
2025-02-21 17:35:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ document.querySelector('button').addEventListener('click',myClick);
 
 let a;
 
+// Вписывает значение в a
+
 function myClick() {
     console.log('work');
     a = document.querySelector('.search').value;
     console.log(a);
     document.querySelector('.out').innerHTML = a;
 }
+
+// Убирает обновление при Enter
 
 document.getElementById('myInput').addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
